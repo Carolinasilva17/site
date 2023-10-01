f7ed23c6ff6b9c9aaf1c2f37ef7b9e3d9277e79b
Carolinasilva17 <99693031+Carolinasilva17@users.noreply.github.com>
2023-10-01 13:38:35 -0300

    Add files via upload

---
+++ site/config.php
@@ -0,0 +1,7 @@
+<?php
+define('SHEEP_URL', 'localhost/site'); //ou dominio exemplo: maykonsilveira.com.br
+define('SHEEP_HOST', 'localhost');
+define('SHEEP_USER', 'root');
+define('SHEEP_SENHA', '');
+define('SHEEP_BD', 'pagamento');
+?>
---
+++ site/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta cherset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Verinha Leal Crochê</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="center">
+            <div class="logo"> <img src="img/Verinha Leal.png" width="15%">
+            </div><!--logo-->
+            <ul class="menu">
+
+                <li>
+                    <a href="início.html">Inicio</a>
+                </li>
+                <li>
+                    <a href="Sobre.html">Sobre</a>
+                </li>
+                <li>
+                    <a href="Compra.html">Compra</a>
+                </li>
+                <li>
+                    <a href="Contato.html">Contato</a>
+                </li>
+            </ul>
+            </ul>
+        </div><!--center-->
+    </header>
+    <h1>Seja bem-vindo ao meu site</h1>
+    <h1>Verinha leal crochê</h1>
+    <h3>Aqui você verá meus trabalhos feitos com muito amor</h3>
+    <footer class="footer">
+        <div class="container">
+            <div class="links">
+                <a href="https://instagram.com/verinha_lealcroche?igshid=NjlwNzlyMDk2Mg==" target="_blank"> <i
+                        class="fa fa-instagram"></i></a>
+                <a href="https://www.facebook.com/veraney.lealcosta" target="_blank"> <i class="fa fa-facebook"></i></a>
+                <a href="https://wa.me/559185887471" target="_blank"><i class="fa fa-whatsapp"></i></a>
+            </div>
+        </div>
+    </footer>
+    <a class="btn-menu1" href="Compra.html">Compra</a>
+    <a class="btn-menu2" href="Contato.html">Contato</a>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
